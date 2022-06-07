05815adb4465837ff4cb94e98f7ecb04cb1f8417
Crowdbotics <team@crowdbotics.com>
2022-06-07 17:00:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "la_sierra_mobile_35635",
+  url: "https://la_sierra_mobile_35635.botics.co",
+  api: "https://la_sierra_mobile_35635.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
